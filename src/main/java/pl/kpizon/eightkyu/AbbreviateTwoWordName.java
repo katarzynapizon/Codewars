0875bc56cc3cde659053456b7bf865d0c519f661
kasia.p <kasia.rozwalka@gmail.com>
2022-07-08 13:50:43 +0200

    AbbreviateTwoWordName solution and best practice

--- src/main/java/pl/kpizon/eightkyu/AbbreviateTwoWordName.java
+++ src/main/java/pl/kpizon/eightkyu/AbbreviateTwoWordName.java
@@ -11,6 +11,21 @@ patrick feeney => P.F
 public class AbbreviateTwoWordName {
 
     public static String abbrevName(String name) {
+        String[] split = name.toUpperCase().split(" ");
+        for (int i = 0; i < split.length; i++) {
+            if (i == 0) {
+                name = String.valueOf(split[i].charAt(0));
+            }
+            if (i == 1) {
+                name = name.concat(".").concat(String.valueOf(split[i].charAt(0)));
+            }
+        }
         return name;
+    }
+
+    public static String abbrevName2(String name) {
+        String[] names = name.split(" ");
+        return (names[0].charAt(0) + "." + names[1].charAt(0))
+                .toUpperCase();
     }
 }
