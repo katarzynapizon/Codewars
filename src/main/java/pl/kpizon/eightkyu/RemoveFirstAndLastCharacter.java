65cf4a5aa2f25c8b53b077698a6eca7e73189794
kasia.p <kasia.rozwalka@gmail.com>
2022-07-11 20:55:44 +0200

    RemoveFirstAndLastCharacter test and instruction

---
+++ src/main/java/pl/kpizon/eightkyu/RemoveFirstAndLastCharacter.java
@@ -0,0 +1,14 @@
+package pl.kpizon.eightkyu;
+
+/*
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+You're given one parameter, the original string.
+You don't have to worry with strings with less than two characters.
+ */
+
+public class RemoveFirstAndLastCharacter {
+
+    public static String remove(String str) {
+        return "";
+    }
+}
